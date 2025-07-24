dca28edb788c970e682f5527afbffb5181b53ca4
Jimmyn <2464344891@qq.com>
2025-07-25 00:29:25 +0800

    情绪日记本 1、登录管理： 账号注册、登录、信息修改（用户账号，密码）、账号删除（非登录情况可操作）。

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="global-bg">
     <router-view></router-view>
   </div>
 </template>
 
-
 <style scoped>
-  .el-input {
-    width: 180px;
-  }
-  :deep(.el-input__wrapper) {
-    border: none;
-  }
+.global-bg {
+  min-height: 100vh;
+  background: url('./static/imgs/back.png') no-repeat center center fixed;
+  background-size: cover;
+}
+.el-input {
+  width: 180px;
+}
+:deep(.el-input__wrapper) {
+  border: none;
+}
 </style>
